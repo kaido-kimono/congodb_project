a29026abe95889a18c031b2e56dd123ce7d875e2
Mayeze Showee <showeemayeze@outlook.fr>
2019-05-17 15:37:13 +0200

    modification du constructeur de la classe Champ

--- DB/src/Champ.java
+++ DB/src/Champ.java
@@ -3,41 +3,37 @@ public class Champ <F> {
     Donnee <F> _donnee;
     String _nom_champ;
 
-    public Champ(String nom_champ, int donnee){
-        this._donnee = new <Integer> Donnee(donnee);
+    public Champ(String nom_champ, F donnee){
+
         this._nom_champ = nom_champ;
-    }
-    public Champ(String nom_champ,String donnee){
-        this._donnee=new <String> Donnee(donnee);
-        this._nom_champ = nom_champ;
-    }
-    public Champ(String nom_champ,double donnee){
-        this._donnee=new<Double> Donnee(donnee);
-        this._nom_champ = nom_champ;
-    }
-    public Champ(String nom_champ,boolean donnee ){
-        this._donnee = new <Boolean> Donnee(donnee);
-        this._nom_champ = nom_champ;
-    }
-    public Champ(String nom_champ,char donnee){
-        this._donnee = new<Character> Donnee(donnee);
-        this._nom_champ = nom_champ;
-    }
-    public Champ(String nom_champ,float donnee){
-        this._donnee = new<Float> Donnee(donnee);
-        this._nom_champ = nom_champ;
-    }
-    public Champ(String nom_champ,long donnee){
-        this._donnee = new<Long> Donnee(donnee);
-        this._nom_champ = nom_champ;
-    }
-    public Champ(String nom_champ,byte donnee){
-        this._donnee = new<Byte> Donnee(donnee);
-        this._nom_champ = nom_champ;
-    }
-    public Champ(String nom_champ,short donnee){
-        this._donnee = new<Short> Donnee(donnee);
-        this._nom_champ = nom_champ;
+
+        if (donnee instanceof Integer){
+            this._donnee = new <Integer> Donnee(donnee);
+        }
+        else if(donnee instanceof Double){
+            this._donnee=new<Double> Donnee(donnee);
+        }
+        else if (donnee instanceof String){
+            this._donnee=new <String> Donnee(donnee);
+        }
+        else if (donnee instanceof Boolean){
+            this._donnee = new <Boolean> Donnee(donnee);
+        }
+        else if (donnee instanceof Character){
+            this._donnee = new<Character> Donnee(donnee);
+        }
+        else if (donnee instanceof Float){
+            this._donnee = new<Float> Donnee(donnee);
+        }
+        else if (donnee instanceof Long){
+            this._donnee = new<Long> Donnee(donnee);
+        }
+        else if (donnee instanceof Byte){
+            this._donnee = new<Byte> Donnee(donnee);
+        }
+        else if (donnee instanceof Short){
+            this._donnee = new<Short> Donnee(donnee);
+        }
     }
 
     public Donnee get_donnee() {
