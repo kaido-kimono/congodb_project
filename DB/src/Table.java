db556dcd830e6b16d5ee610cf767dec705927aee
Mayeze Showee <showeemayeze@outlook.fr>
2019-05-17 11:19:23 +0200

    Ajout de l'attribut clesecondaire dans la classe Table

--- DB/src/Table.java
+++ DB/src/Table.java
@@ -4,6 +4,7 @@ public class Table {
     List<Champ> champs;
     String _nom_table;
     Champ _cleprimaire = null;
+    Champ _clesecondaire = null;
 
     public Table(){this._nom_table="Table";}
 }
