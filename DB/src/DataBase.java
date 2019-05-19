e924c0f83c57154ab2a92db909942d7c885fd90c
Mayeze Showee <showeemayeze@outlook.fr>
2019-05-19 09:05:33 +0200

    ajout de la fonctionnalité CreerTable et SupprimerTable

--- DB/src/DataBase.java
+++ DB/src/DataBase.java
@@ -11,6 +11,18 @@ public class DataBase {
         this._nom_utilisateur = nomUser;
     }
 
-
+    public void creerTable(){
+        Table table = new Table();
+        this.tables.add(table);
+    }
+    public void creerTable(String nom_table){
+        Table table = new Table(nom_table);
+        this.tables.add(table);
+    }
+    public void SuppimerTable(String nom_table){
+        tables.forEach(table -> {if(table._nom_table==nom_table){
+        this.tables.remove(table);}
+        });
+    }
 
 }
