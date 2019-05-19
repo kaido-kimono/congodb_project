3bf360b33526420930c861c2437d868b32b6e977
Mayeze Showee <showeemayeze@outlook.fr>
2019-05-19 10:02:59 +0200

    ajout d'un objet outputstream "save" initialiser a null

--- DB/src/Serialisation.java
+++ DB/src/Serialisation.java
@@ -1,7 +1,9 @@
+import java.io.ObjectOutputStream;
 import java.util.List;
 
 public class Serialisation <T>{
     public void serialise(List<T> L, String nom_fichier)throws ClassNotFoundException{
+        ObjectOutputStream save = null;
 
     }
 }
