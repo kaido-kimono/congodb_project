7e1733b5b43fa77da5dec387db3d1c7938111471
Mayeze Showee <showeemayeze@outlook.fr>
2019-05-19 10:06:32 +0200

    ajout d'un objet de type FileOutputStream dans un try-catch

--- DB/src/Serialisation.java
+++ DB/src/Serialisation.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.List;
 
@@ -5,5 +7,10 @@ public class Serialisation <T>{
     public void serialise(List<T> L, String nom_fichier)throws ClassNotFoundException{
         ObjectOutputStream save = null;
 
+        try{
+            final FileOutputStream fichier = new FileOutputStream(nom_fichier);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
