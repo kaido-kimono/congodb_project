cfaa6cc6cb792e087bc2c96d757e6c0393a4e43b
Mayeze Showee <showeemayeze@outlook.fr>
2019-05-19 10:01:36 +0200

    creation de la methode serialise

--- DB/src/Serialisation.java
+++ DB/src/Serialisation.java
@@ -1,2 +1,7 @@
-public class Serialisation {
+import java.util.List;
+
+public class Serialisation <T>{
+    public void serialise(List<T> L, String nom_fichier)throws ClassNotFoundException{
+
+    }
 }
